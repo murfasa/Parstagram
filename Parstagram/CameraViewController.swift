10b07d6e7b3d5bd2890e0ed5e27754f62bc8e576
Murtaza <murfasa@users.noreply.github.com>
2021-10-17 22:43:51 -0400

    Retain logins + logout button

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -27,13 +27,14 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         post["author"] = PFUser.current()!
         
         let imageData = imageView.image!.pngData()
-        let file = PFFileObject(data: imageData!)
+        let file = PFFileObject(name: "image.png", data: imageData!)
         
         post["image"] = file
         
         post.saveInBackground { (success, error) in
             if success {
-                    
+                print("saved!")
+                self.dismiss(animated: true, completion: nil)
             } else {
                 print("Error:  \(String(describing: error?.localizedDescription))")
             }
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -27,7 +27,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         
-        let query = PFQuery(className:"Posts")
+        let query = PFQuery(className: "Posts")
         query.includeKey("author")
         query.limit = 20
         
@@ -36,6 +36,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
                 self.posts = posts!
                 self.tableView.reloadData()
             }
+            print(error ?? "reloaded")
         }
     }
     
@@ -61,7 +62,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         return cell
     }
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
